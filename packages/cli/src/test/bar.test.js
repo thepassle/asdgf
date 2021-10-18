15c30e2902a87787e588870dab5184545340c2f3
thepassle <pascalschilp@gmail.com>
2021-10-18 09:41:12 +0200

    test

---
+++ packages/cli/src/test/bar.test.js
@@ -0,0 +1,8 @@
+import { describe } from '@asdgf/core';
+
+const sum = (a,b) => a+b;
+
+describe('bar.test', ({it}) => {
+  it('adds two numsell as well', () => {
+  })
+});
---
+++ packages/cli/src/test/foo.test.js
@@ -0,0 +1,9 @@
+import { describe } from '@asdgf/core';
+
+const sum = (a,b) => a+b;
+
+describe('fooasadfsdf.test', ({it}) => {
+  it('adds two nums', () => {
+    // throw new Error();
+  })
+});
